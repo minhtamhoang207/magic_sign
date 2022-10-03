4bc7243c5d22fadb09927080e276dcb9557d0894
TamHM <tamhm@mor.com.vn>
2022-10-04 02:56:57 +0700

    save code

--- lib/app/pages/splash/controllers/splash_controller.dart
+++ lib/app/pages/splash/controllers/splash_controller.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:magic_sign/data/models/coffee.dart';
 import 'package:magic_sign/domain/usecases/sample_usecases.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../../routes/app_pages.dart';
 
 class SplashController extends GetxController with StateMixin<SplashController>{
@@ -8,12 +14,21 @@ class SplashController extends GetxController with StateMixin<SplashController>{
   SplashController({required this.sampleUseCases});
 
   final count = 0.obs;
+  final Rx<List<int>> bytes = Rx<List<int>>([]);
   @override
   void onInit() async {
-    final List<Coffee> icedCoffee = await sampleUseCases.getListIcedCoffee();
-    await Future.delayed(const Duration(seconds: 2));
-    Get.offAllNamed(Routes.INTRO);
+    bytes.value = await getSth();
+    // final List<Coffee> icedCoffee = await sampleUseCases.getListIcedCoffee();
+    // await Future.delayed(const Duration(seconds: 2));
+    // Get.offAllNamed(Routes.INTRO);
     super.onInit();
+  }
+
+  Future<List<int>> getSth() async {
+    String txt = 'Hello there';
+    final value = await Dio().post('https://aae7-2405-4802-13a-1ba0-fcba-a511-a550-fddc.ap.ngrok.io/api/v1/convertToSign?txt=$txt');
+    List<int> bytes = value.data.codeUnits;
+    return bytes;
   }
 
   @override
--- lib/app/pages/splash/views/splash_view.dart
+++ lib/app/pages/splash/views/splash_view.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:better_player/better_player.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/splash_controller.dart';
@@ -6,15 +11,244 @@ class SplashView extends GetView<SplashController> {
   const SplashView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello Tom hehe',
-          style: TextStyle(
-            fontSize: 25,
-            fontStyle: FontStyle.italic
+    return Scaffold(
+      body:
+      Obx(()=>
+      controller.bytes.value.isEmpty?
+      const Center(
+        child: Text('Loading'),
+      ):
+      MemoryPlayerPage())
+    );
+  }
+}
+
+// class _BumbleBeeRemoteVideo extends StatefulWidget {
+//   @override
+//   _BumbleBeeRemoteVideoState createState() => _BumbleBeeRemoteVideoState();
+// }
+//
+// class _BumbleBeeRemoteVideoState extends State<_BumbleBeeRemoteVideo> {
+//   late VideoPlayerController _controller;
+//
+//   @override
+//   void initState() {
+//     _controller = VideoPlayerController.file(
+//       File(''),
+//       // File(Get.find<DetectSignController>().fileA.path),
+//       videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+//     );
+//
+//     _controller.addListener(() {
+//       setState(() {});
+//     });
+//     _controller.setLooping(true);
+//     _controller.initialize();
+//     super.initState();
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: <Widget>[
+//           Container(padding: const EdgeInsets.only(top: 20.0)),
+//           const Text('With remote mp4'),
+//           Container(
+//             padding: const EdgeInsets.all(20),
+//             child: AspectRatio(
+//               aspectRatio: _controller.value.aspectRatio,
+//               child: Stack(
+//                 alignment: Alignment.bottomCenter,
+//                 children: <Widget>[
+//                   VideoPlayer(_controller),
+//                   ClosedCaption(text: _controller.value.caption.text),
+//                   _ControlsOverlay(controller: _controller),
+//                   VideoProgressIndicator(_controller, allowScrubbing: true),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+// class _ControlsOverlay extends StatelessWidget {
+//   const _ControlsOverlay({Key? key, required this.controller})
+//       : super(key: key);
+//
+//   static const List<Duration> _exampleCaptionOffsets = <Duration>[
+//     Duration(seconds: -10),
+//     Duration(seconds: -3),
+//     Duration(seconds: -1, milliseconds: -500),
+//     Duration(milliseconds: -250),
+//     Duration.zero,
+//     Duration(milliseconds: 250),
+//     Duration(seconds: 1, milliseconds: 500),
+//     Duration(seconds: 3),
+//     Duration(seconds: 10),
+//   ];
+//   static const List<double> _examplePlaybackRates = <double>[
+//     0.25,
+//     0.5,
+//     1.0,
+//     1.5,
+//     2.0,
+//     3.0,
+//     5.0,
+//     10.0,
+//   ];
+//
+//   final VideoPlayerController controller;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: <Widget>[
+//         AnimatedSwitcher(
+//           duration: const Duration(milliseconds: 50),
+//           reverseDuration: const Duration(milliseconds: 200),
+//           child: controller.value.isPlaying
+//               ? const SizedBox.shrink()
+//               : Container(
+//             color: Colors.black26,
+//             child: const Center(
+//               child: Icon(
+//                 Icons.play_arrow,
+//                 color: Colors.white,
+//                 size: 100.0,
+//                 semanticLabel: 'Play',
+//               ),
+//             ),
+//           ),
+//         ),
+//         GestureDetector(
+//           onTap: () {
+//             controller.value.isPlaying ? controller.pause() : controller.play();
+//           },
+//         ),
+//         Align(
+//           alignment: Alignment.topLeft,
+//           child: PopupMenuButton<Duration>(
+//             initialValue: controller.value.captionOffset,
+//             tooltip: 'Caption Offset',
+//             onSelected: (Duration delay) {
+//               controller.setCaptionOffset(delay);
+//             },
+//             itemBuilder: (BuildContext context) {
+//               return <PopupMenuItem<Duration>>[
+//                 for (final Duration offsetDuration in _exampleCaptionOffsets)
+//                   PopupMenuItem<Duration>(
+//                     value: offsetDuration,
+//                     child: Text('${offsetDuration.inMilliseconds}ms'),
+//                   )
+//               ];
+//             },
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(
+//                 // Using less vertical padding as the text is also longer
+//                 // horizontally, so it feels like it would need more spacing
+//                 // horizontally (matching the aspect ratio of the video).
+//                 vertical: 12,
+//                 horizontal: 16,
+//               ),
+//               child: Text('${controller.value.captionOffset.inMilliseconds}ms'),
+//             ),
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.topRight,
+//           child: PopupMenuButton<double>(
+//             initialValue: controller.value.playbackSpeed,
+//             tooltip: 'Playback speed',
+//             onSelected: (double speed) {
+//               controller.setPlaybackSpeed(speed);
+//             },
+//             itemBuilder: (BuildContext context) {
+//               return <PopupMenuItem<double>>[
+//                 for (final double speed in _examplePlaybackRates)
+//                   PopupMenuItem<double>(
+//                     value: speed,
+//                     child: Text('${speed}x'),
+//                   )
+//               ];
+//             },
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(
+//                 // Using less vertical padding as the text is also longer
+//                 // horizontally, so it feels like it would need more spacing
+//                 // horizontally (matching the aspect ratio of the video).
+//                 vertical: 12,
+//                 horizontal: 16,
+//               ),
+//               child: Text('${controller.value.playbackSpeed}x'),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+
+class MemoryPlayerPage extends StatefulWidget {
+  @override
+  _MemoryPlayerPageState createState() => _MemoryPlayerPageState();
+}
+
+class _MemoryPlayerPageState extends State<MemoryPlayerPage> {
+  late BetterPlayerController _betterPlayerController;
+
+  @override
+  void initState() {
+    BetterPlayerConfiguration betterPlayerConfiguration =
+    BetterPlayerConfiguration(
+      aspectRatio: 16 / 9,
+      fit: BoxFit.contain,
+    );
+
+    _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
+    _setupDataSource();
+    super.initState();
+  }
+
+  void _setupDataSource() async {
+    List<int> bytes = Get.find<SplashController>().bytes.value;
+    BetterPlayerDataSource dataSource =
+    BetterPlayerDataSource.memory(bytes, videoExtension: "mp4");
+    _betterPlayerController.setupDataSource(dataSource);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Memory player"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Memory player with plays video from bytes list. In this example"
+                  "file bytes are read to list and then used in player.",
+              style: TextStyle(fontSize: 16),
+            ),
           ),
-        ),
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: BetterPlayer(controller: _betterPlayerController),
+          ),
+        ],
       ),
     );
   }
